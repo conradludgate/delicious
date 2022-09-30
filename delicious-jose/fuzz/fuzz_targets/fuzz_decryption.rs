24169c2e607414eafb8b22fb3367cb95a7f1a470
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-09-30 09:43:12 +0100

    fuzzy

--- delicious-jose/fuzz/fuzz_targets/fuzz_decryption.rs
+++ delicious-jose/fuzz/fuzz_targets/fuzz_decryption.rs
@@ -1,27 +1,21 @@
 #![no_main]
 #[macro_use]
 extern crate libfuzzer_sys;
-extern crate delicious_jose;
-extern crate serde_json;
 
-use delicious_jose::jwa::{ContentEncryptionAlgorithm, KeyManagementAlgorithm};
-use delicious_jose::jwk::JWK;
-use delicious_jose::{Compact, JWE};
+use delicious_jose::jwa::{cea, kma};
+use delicious_jose::jwe::Encrypted;
+use delicious_jose::jwk;
 
 fuzz_target!(|data: &[u8]| {
-    let key = JWK::new_octet_key(&vec![0; 256 / 8], Default::default());
+    let key = jwk::OctetKey::new(vec![0; 256 / 8]);
 
     let token = match std::str::from_utf8(data) {
-        Ok(t) => t,
+        Ok(token) => token,
         Err(_) => return,
     };
-
-    let token = Compact::decode(token);
-
-    let _ = JWE::decrypt(
-        &token,
-        &key,
-        KeyManagementAlgorithm::A256GCMKW,
-        ContentEncryptionAlgorithm::A256GCM,
-    );
+    let token: Encrypted<kma::A256GCMKW> = match token.parse() {
+        Ok(token) => token,
+        Err(_) => return,
+    };
+    let _ = token.decrypt::<(), cea::A256GCM>(&key);
 });
--- delicious-jose/fuzz/fuzz_targets/fuzz_signature_validation.rs
+++ delicious-jose/fuzz/fuzz_targets/fuzz_signature_validation.rs
@@ -1,21 +1,21 @@
 #![no_main]
 #[macro_use]
 extern crate libfuzzer_sys;
-extern crate delicious_jose;
-extern crate serde_json;
 
-use delicious_jose::*;
-use delicious_jose::jws::*;
-use delicious_jose::jwa::*;
+use delicious_jose::jwa::sign;
+use delicious_jose::jwk;
+use delicious_jose::jws::{Decoded, Encoded};
 
 fuzz_target!(|data: &[u8]| {
-    let signing_secret = Secret::Bytes("secret".to_string().into_bytes());
-    
+    let key = jwk::OctetKey::new(vec![0; 256 / 8]);
+
     let token = match std::str::from_utf8(data) {
-        Ok(t) => t,
+        Ok(token) => token,
         Err(_) => return,
     };
-
-    let token = Compact::decode(token);
-    let _ = JWT::<serde_json::Value, delicious_jose::()>::decode(&token, &signing_secret, SignatureAlgorithm::HS256);
+    let token: Encoded<()> = match token.parse() {
+        Ok(token) => token,
+        Err(_) => return,
+    };
+    let _ = Decoded::<()>::decode::<sign::HS256>(token, &key);
 });
